60b88a650369c645bcebf64c843a8b24d6a73df4
varun8100 <95116684+varun8100s@users.noreply.github.com>
2022-12-11 00:47:45 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 
-const socket = io(`https://retrochatserver.onrender.com}`)
+const socket = io(`https://retrochatserver.onrender.com/`)
 const msgForm = document.getElementById('sendContainer')
 const msgInput = document.getElementById('messageInput')
 const msgContainer = document.getElementById('messageContainer')
