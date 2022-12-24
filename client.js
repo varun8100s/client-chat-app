feb53f8134cb86ef4df17a232c7f1cfb1afb120f
varun8100 <95116684+varun8100s@users.noreply.github.com>
2022-12-24 20:38:55 +0530

    Update client.js

--- client.js
+++ client.js
@@ -3,7 +3,9 @@ const msgForm = document.getElementById('sendContainer')
 const msgInput = document.getElementById('messageInput')
 const msgContainer = document.getElementById('messageContainer')
 const errorContainer = document.getElementById('error-container')
-
+const api = "a61a1e437ff97e646b8d5bde849927fd61e8bbe8f7517e40569a1370"
+let IP, clientId
+getClientIp()
     
     socket.on('client-connected', data => {
         console.log(data)
@@ -62,3 +64,23 @@ const theElement = msgContainer
 const scrollToBottom = (node) => {
 	node.scrollTop = node.scrollHeight;
 }
+
+function getClientIp() {
+    let request = new XMLHttpRequest();
+    let response;
+    request.open('GET', `https://api.ipdata.co/?api-key=${api}`);
+
+    request.setRequestHeader('Accept', 'application/json');
+
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            IP = this.responseText
+            socket.emit('client-ip',{id:clientId, ip:IP})
+            
+        }
+    };
+    
+    request.send();
+}
+
+
