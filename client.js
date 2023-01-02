6109a1896ac5fab714a328459b2616e64805aa75
varun8100 <95116684+varun8100s@users.noreply.github.com>
2023-01-03 03:13:25 +0530

    refactored code

--- client.js
+++ client.js
@@ -1,88 +1,67 @@
-const socket = io(`https://retrochatserver.onrender.com`);
-const msgForm = document.getElementById('sendContainer')
-const msgInput = document.getElementById('messageInput')
-const msgContainer = document.getElementById('messageContainer')
-const sendBtn = document.getElementById("sendButton");
-const errorContainer = document.getElementById("errorContainer");
-const typing = document.getElementById('typing')
+const socket = io('https://retrojchatserver.onrender.com');
+const msgForm = document.getElementById('sendContainer');
+const msgInput = document.getElementById('messageInput');
+const msgContainer = document.getElementById('messageContainer');
+const sendBtn = document.getElementById('sendButton');
+const errorContainer = document.getElementById('errorContainer');
+const typing = document.getElementById('typing');
 
-    socket.on("client-connected", (data) => {
-      console.log(data);
-      errorContainer.innerHTML = `<span style="color:rgb(0,250,250);">${data}</span><br><span style="color:white">Current User ID:</span> <span style="color:pink;">${socket.id}</span>`;
-    }); 
+// Use object destructuring to extract data from event object
+socket.on('client-connected', (data) => {
+  errorContainer.innerHTML = `<span style="color:rgb(0,250,250);">${data}</span><br><span style="color:white">Current User ID:</span> <span style="color:pink;">${socket.id}</span>`;
+}); 
 
-
-socket.on("broadcast", (data) => {
-  console.log(data);
-  let messageElement = document.createElement("div");
-  messageElement.className = "message-received";
-  let span = document.createElement('span')
-  span.innerText = data.msg
-  span.style.color="white"
-  messageElement.innerText = data.id+" said:\n";
+// Use object destructuring to extract data from event object
+socket.on('broadcast', ({ id, message }) => {
+  console.log({ id, message });
+  const messageElement = document.createElement('div');
+  messageElement.className = 'message-received';
+  const span = document.createElement('span');
+  span.innerText = message;
+  span.style.color = 'white';
+  messageElement.innerText = `${id} said:\n`;
   msgContainer.append(messageElement);
-  msgContainer.append(span)
+  msgContainer.append(span);
 
+  // Scroll to bottom of message container after appending new message
   msgContainer.scrollTop = msgContainer.scrollHeight;
 });
 
-msgForm.addEventListener("submit", (event) => {
+msgForm.addEventListener('submit', event => {
+  // Prevent default form submit behavior
   event.preventDefault();
   const message = msgInput.innerText.trim();
 
   if (message.length > 500) {
-    console.log("limit exceeded 500 characters")
-    sendBtn.style.color = 'pink'
-    sendBtn.innerText="Sending your moms measurements? 😂"
+    console.log('limit exceeded 500 characters');
+    sendBtn.style.color = 'pink';
+    sendBtn.innerText = 'Sending your moms measurements? 😂';
     setTimeout(() => {
-      sendBtn.innerText = "Send !"    
-      sendBtn.style.color = 'white'
-    }, 3000)
-
-    return;
-  }
-
-  if (message != "") {
-    socket.emit("send-client-message", { id: socket.id, msg: message });
-    
-      let messageElement = document.createElement('div')
-      messageElement.className = 'message-sent'
-    messageElement.innerText = message
-      msgContainer.append(messageElement)
-      
-      msgInput.innerText = "";
-      msgContainer.scrollTop = msgContainer.scrollHeight;
-    
-  } else {
-    sendBtn.style.color = "pink";
-    sendBtn.innerText = "Type something you retard 🤓";
-    setTimeout(() => {
-      sendBtn.innerText = "Send !";
-      sendBtn.style.color = "white";
+      sendBtn.innerText = 'Send !';    
+      sendBtn.style.color = 'white';
     }, 3000);
-
     return;
   }
-});
 
-
-
-msgInput.addEventListener("input", (event) => {
-  console.log("typing");
-  socket.emit("typing", socket.id);
-});
-
-
-let userTimeout = null;
-socket.on("someone-typing", (who) => {
-  
-  typing.style = "content-visibility: visible";
-  typing.innerText = who + " is typing ... "
-  if (userTimeout != null) clearTimeout(userTimeout);
-  userTimeout = setTimeout(() => {
-    typing.innerText = ""
-    typing.style = "content-visibility: hidden";
-    userTimeout = null;
-  },3000)
- 
+  if (message !== '') {
+    // Emit 'send-client-message' event to server with id and message data
+    socket.emit('send-client-message', { id: socket.id, message });
+    
+    const messageElement = document.createElement('div');
+    messageElement.className = 'message-sent';
+    messageElement.innerText = message;
+    msgContainer.append(messageElement);
+    
+    // Clear input field and scroll to bottom of message container after sending message
+    msgInput.innerText = '';
+    msgContainer.scrollTop = msgContainer.scrollHeight;
+  } else {
+    sendBtn.style.color = 'pink';
+    sendBtn.innerText = 'Type something you retard 🤓';
+    setTimeout(() => {
+      sendBtn.innerText = 'Send !';
+      sendBtn.style.color = 'white';
+    }, 3000);
+    return;
+  }
 });
--- script.js
+++ script.js
@@ -1,42 +1,32 @@
+const MIN_FONT_SIZE = 1;
+
 function handleKeyUp(event) {
-
-  let BASE_FONT_SIZE = 1;
-  let MIN_FONT_SIZE = 1;
-  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-  if (vw < 1000) {
-    BASE_FONT_SIZE = 2
-    MIN_FONT_SIZE = 2
-  } 
-
   const input = event.target;
-  
   const inputLength = input.innerText.length;
-  const inputFontSize = input.style.fontSize;
+  const viewportWidth = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+  const BASE_FONT_SIZE = viewportWidth < 1000 ? 2 : 1;
 
-  if (input.innerText[inputLength - 1] == '\n') {
-    document.getElementById('sendButton').click()
+  if (input.innerText[inputLength - 1] == "\n") {
+    document.getElementById("sendButton").click();
   }
 
-  let newFontSize = BASE_FONT_SIZE;
-
-  if (inputLength > BASE_FONT_SIZE) {
-    newFontSize = BASE_FONT_SIZE - Math.ceil(inputLength / BASE_FONT_SIZE);
-  }
-
-  input.style.fontSize =
-    newFontSize < MIN_FONT_SIZE ? `${MIN_FONT_SIZE}rem` : `${newFontSize}rem`;
+  const newFontSize = Math.max(
+    MIN_FONT_SIZE,
+    BASE_FONT_SIZE - Math.ceil(inputLength / BASE_FONT_SIZE)
+  );
+  input.style.fontSize = `${newFontSize}rem`;
 }
 
 function handleBlur(event) {
   const input = event.target;
-
   if (!input.innerText.trim().length) {
     input.innerHTML = "";
   }
 }
 
 const input = document.querySelector(".input");
-
 input.addEventListener("keyup", handleKeyUp);
 input.addEventListener("blur", handleBlur);
-
